a121378b1e612ef220b0bd6cd7cb461ac28be29d
jamesball27 <jwrb27@gmail.com>
2017-06-30 19:07:57 -0400

    Add CitymapperApi helper class

--- lib/citymapper_api.rb
+++ lib/citymapper_api.rb
@@ -1,4 +1,21 @@
 
 class CitymapperApi
+
   include 'HTTParty'
+
+  def self.get_travel_time(start_coord, end_coord, time)
+    url = "https://developer.citymapper.com"
+    
+    params = {
+      startcood: start_coord,
+      endcoord: end_coord,
+      time: time,
+      time_type: "arrival",
+      key: ENV["citymapper_api_key"]
+    }
+
+    response = JSON.parse get(url, query: params).to_s
+    response["travel_time_minutes"]
+  end
+
 end
