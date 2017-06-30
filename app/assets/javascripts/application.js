408c8614a226560510d3b076106be84d348d1a26
jamesball27 <jwrb27@gmail.com>
2017-06-30 16:24:14 -0400

    Build basic form with ability to add destinations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,33 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = document.getElementById('starting-location');
+  new google.maps.places.Autocomplete(start);
+
+  const appendNewAutocomplete = (e) => {
+    e.preventDefault();
+
+    const inputs = document.querySelectorAll('.autocomplete');
+    const inputNumber = inputs.length;
+    const form = document.getElementById('form');
+    const submit = document.getElementById('submit');
+
+    const label = document.createElement('label');
+    const labelText = document.createTextNode(`Destination ${inputNumber}`);
+    label.appendChild(labelText);
+    const input = document.createElement('input');
+    input.id = `input-${ inputs.length + 1}`;
+    input.className = 'autocomplete';
+    label.htmlFor = input.id;
+
+    form.insertBefore(label, submit);
+    form.insertBefore(input, submit);
+    form.insertBefore(document.createElement('br'), submit);
+    new google.maps.places.Autocomplete(input);
+  };
+
+  const addDestination = document.getElementById('add-destination');
+  addDestination.addEventListener('click', appendNewAutocomplete);
+});
---
+++ app/controllers/travel_times_controller.rb
@@ -0,0 +1,9 @@
+class TravelTimesController < ApplicationController
+
+  def show
+  end
+
+  def create
+  end
+
+end
